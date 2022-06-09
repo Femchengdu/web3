0a559ae4a1a348cfecf5707ab642f44b0cdd2e69
Samuel Ajayi <femiajayi705@gmail.com>
2022-06-09 10:12:23 +0200

    refac: Correcting sytax errors

--- DAO/smartcontracts/contracts/Dao.sol
+++ DAO/smartcontracts/contracts/Dao.sol
@@ -1,10 +1,8 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.07;
-
-import "hardhat/console.sol";
+pragma solidity ^0.8.14;
 
 interface IdaoContract {
-    function balanceOf(address, uint256) external view returns (uint256)
+    function balanceOf(address, uint256) external view returns (uint256);
 }
 
 contract MoralisDao {
@@ -13,18 +11,20 @@ contract MoralisDao {
     uint256[] public validTokens;
     IdaoContract daoContract;
 
-    constructor(){
+    constructor() {
         owner = msg.sender;
         nextProposal = 1;
-        daoContract = IdaoContract(0x2953399124F0cBB46d2CbACD8A89cF0599974963)
-        validTokens = [26848454517621667175507242112331270997219441727981890049716604364689722310657]
+        daoContract = IdaoContract(0x2953399124F0cBB46d2CbACD8A89cF0599974963);
+        validTokens = [
+            26848454517621667175507242112331270997219441727981890049716604364689722310657
+        ];
     }
 
     struct proposal {
         uint256 id;
-        bool exits;
+        bool exists;
         string description;
-        uint deadline;
+        uint256 deadline;
         uint256 votesUp;
         uint256 votesDown;
         address[] canVote;
@@ -34,7 +34,7 @@ contract MoralisDao {
         bool passed;
     }
 
-    mapping(uint256 => proposal) public Proposal
+    mapping(uint256 => proposal) public Proposals;
 
     event proposalCreated(
         uint256 id,
@@ -48,38 +48,45 @@ contract MoralisDao {
         uint256 votesDown,
         address voter,
         uint256 proposal,
-        bool votedFor,
+        bool votedFor
     );
 
-    event proposalCount(
-        uint256 id,
-        bool passed,
-    );
+    event proposalCount(uint256 id, bool passed);
 
-    function checkProposalEligibility (address _proposalist) private view returns(
-        bool
-    ){
-        for(uint i=0; i< validTokens.length; i++){
-            if(daoContract.balanceOf(_proposalist, validTokens[i]) >=1){
-                return true
-            }
-            return false
-        }
-    }
-    function checkVoteEligibility (uint256 _id, address _voter) private view returns(
-        bool
-    ){
-        for(uint256 i=0; i< Proposals[_id].canVote.length; i++){
-            if(Proposals[_id].canVote[i] == _voter){
-                return true
-            }
-            return false
-        }
-    }
-
-    function createProposal (string memory _description, address[] memory _canVote ) public
+    function checkProposalEligibility(address _proposalist)
+        private
+        view
+        returns (bool)
     {
-        require(checkProposalEligibility(msg.sender), "Only NFT holders can put forth Proposals");    
+        for (uint256 i = 0; i < validTokens.length; i++) {
+            if (daoContract.balanceOf(_proposalist, validTokens[i]) >= 1) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    function checkVoteEligibility(uint256 _id, address _voter)
+        private
+        view
+        returns (bool)
+    {
+        for (uint256 i = 0; i < Proposals[_id].canVote.length; i++) {
+            if (Proposals[_id].canVote[i] == _voter) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    function createProposal(
+        string memory _description,
+        address[] memory _canVote
+    ) public {
+        require(
+            checkProposalEligibility(msg.sender),
+            "Only NFT holders can put forth Proposals"
+        );
 
         proposal storage newProposal = Proposals[nextProposal];
         newProposal.id = nextProposal;
@@ -89,21 +96,35 @@ contract MoralisDao {
         newProposal.canVote = _canVote;
         newProposal.maxVotes = _canVote.length;
 
-        emit proposalCreated(nextProposal, _description, _canVote.length, msg.sender);
-        nextProposal++
+        emit proposalCreated(
+            nextProposal,
+            _description,
+            _canVote.length,
+            msg.sender
+        );
+        nextProposal++;
     }
 
     function voteOnProposal(uint256 _id, bool _vote) public {
-        require(Poroposals[_id].exists, "This Proposal does not exist");    
-        require(checkProposalEligibility(msg.sender), "You can not vote on this Proposal");    
-        require(!Proposals[_id].voteStatus[msg.sender], "You have already voted for this Proposals");    
-        require(block.number <= Proposals[_id].deadline, "The deadline has passed for this Proposal");    
+        require(Proposals[_id].exists, "This Proposal does not exist");
+        require(
+            checkProposalEligibility(msg.sender),
+            "You can not vote on this Proposal"
+        );
+        require(
+            !Proposals[_id].voteStatus[msg.sender],
+            "You have already voted for this Proposals"
+        );
+        require(
+            block.number <= Proposals[_id].deadline,
+            "The deadline has passed for this Proposal"
+        );
 
         proposal storage p = Proposals[_id];
 
-        if(_vote){
+        if (_vote) {
             p.votesUp++;
-        }else{
+        } else {
             p.votesDown++;
         }
 
@@ -113,28 +134,28 @@ contract MoralisDao {
     }
 
     function countVotes(uint256 _id) public {
-        require(msg.sender == owner, "Only Owner Can Count Votes");    
-        require(Proposals[_id].exists, "This Proposal does not exist");    
-        require(block.number > Proposals[_id].deadline, "Voting has not concluded");    
-        require(!Proposals[_id].countConducted, "Count already conducted");    
+        require(msg.sender == owner, "Only Owner Can Count Votes");
+        require(Proposals[_id].exists, "This Proposal does not exist");
+        require(
+            block.number > Proposals[_id].deadline,
+            "Voting has not concluded"
+        );
+        require(!Proposals[_id].countConducted, "Count already conducted");
 
         proposal storage p = Proposals[_id];
 
-        if(Proposals[_id].votesDown < Proposals[_id].votesUp){
-            p.passed = true
+        if (Proposals[_id].votesDown < Proposals[_id].votesUp) {
+            p.passed = true;
         }
 
         p.countConducted = true;
 
-
-        emit proposalCount(_id, p.passed );
-
+        emit proposalCount(_id, p.passed);
     }
 
     function addTokenId(uint256 _tokenId) public {
-        require(msg.sender == owner, "Only Owner Can Add Tokens"); 
+        require(msg.sender == owner, "Only Owner Can Add Tokens");
 
-        validTokens.push(_tokenId)
-
+        validTokens.push(_tokenId);
     }
 }
