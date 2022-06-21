368539ebbf795f6c575dc0dd4c720da9d99ac88c
Samuel Ajayi <femiajayi705@gmail.com>
2022-06-21 07:01:33 +0200

    chore: Initial commit for hodl bank front end

---
+++ hodlbank-front/src/components/HodlDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function HodlDetails() {
+
+    return (
+        <>
+            <div>Add hodl info here</div>
+        </>
+    );
+}
+
---
+++ hodlbank-front/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from 'web3uikit';
+
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <MoralisProvider appId="xxx" serverUrl="xxx">
+            <NotificationProvider>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </NotificationProvider>
+        </MoralisProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
